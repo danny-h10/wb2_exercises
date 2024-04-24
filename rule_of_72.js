12278ae262261e62df67d8d9b3edf95b1e44f630
Daniel Hernandez <dh893177@gmail.com>
2024-04-24 13:38:42 -0500

    finished savings

---
+++ rule_of_72.js
@@ -0,0 +1,9 @@
+let savings = 100
+let rate = 8;
+let ruleSeventyTwo = 72;
+let endGoal = 200
+
+
+let years = ruleSeventyTwo / rate
+
+console.log("at a " + rate + "% interest rate, your savings account will be worth " + endGoal + " in " + years + " years")
