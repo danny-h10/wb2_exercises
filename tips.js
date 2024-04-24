51bcb05763f26ede9b1d5a0c57df625e06d5b07f
Daniel Hernandez <dh893177@gmail.com>
2024-04-24 10:58:52 -0500

    added tips

---
+++ tips.js
@@ -0,0 +1,10 @@
+//variable created as inputs for calculation
+let foodCost = 79.25; 
+let tax = 6.54;
+let tip = 12.00;
+
+//do our calculations for the variables above
+let totalDue = foodCost + tax + tip;
+
+//output the results to the console  
+console.log("the total cost is " + totalDue)
