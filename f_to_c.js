083a48f991dbf2058aed70b7ad4592e0fc5a7a9b
Daniel Hernandez <dh893177@gmail.com>
2024-04-25 09:26:43 -0500

    finished f to c

---
+++ f_to_c.js
@@ -0,0 +1,5 @@
+let temp = 59;
+
+let fahrenBeCel = (temp - 32) * 5/9
+
+console.log("the conversion from f to c is " + fahrenBeCel)
--- rule_of_72.js
+++ rule_of_72.js
@@ -6,4 +6,4 @@ let endGoal = savings * 2
 
 let years = ruleSeventyTwo / rate
 
-console.log("at a " + rate + "% interest rate, your savings account will be worth " + endGoal + " in " + years + " years")
+console.log("at an " + rate + "% interest rate, your savings account will be worth " + endGoal + " in " + years + " years")
