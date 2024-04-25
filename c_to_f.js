b8ed5a979be4609aa598da0099895a8c781bd936
Daniel Hernandez <dh893177@gmail.com>
2024-04-25 09:31:15 -0500

    finished c to f

---
+++ c_to_f.js
@@ -0,0 +1,5 @@
+let temp = 20;
+
+let celBeFahren = (temp * 9/5) + 32;
+
+console.log("the conversion from c to f is " + celBeFahren)
--- f_to_c.js
+++ f_to_c.js
@@ -1,5 +1,5 @@
 let temp = 59;
 
-let fahrenBeCel = (temp - 32) * 5/9
+let fahrenBeCel = (temp - 32) * 5/9;
 
 console.log("the conversion from f to c is " + fahrenBeCel)
